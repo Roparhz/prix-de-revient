9f517b23f87fa63d55d2ee0ff2730fc067063e55
Roparhz <julien.leveque21@gmail.com>
2022-09-23 12:49:38 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let afficherPrix = document.querySelector('#content');
+let prixAuKilo = document.querySelector('#prix_au_kg');
+let quantite = document.querySelector('#poids');
+let nom = document.querySelector('#Nom');
+let resulat = 0;
+let total = 0;
+
+function calculPrix (){
+    resulat = (prixAuKilo.value * quantite.value) /1000;
+    total += resulat;
+    
+}
+
+let afficherTotal = document.createElement('div')
+
+document.querySelector('#calculer').addEventListener('click', function(e){
+    e.preventDefault();
+    calculPrix();
+    const afficherResult = document.createElement('div');
+    afficherResult.textContent = nom.value +" "+ resulat + '€';
+    afficherPrix.append(afficherResult, afficherTotal);
+    afficherTotal.textContent = "le total est de : " + total + "€";
+}
+);
